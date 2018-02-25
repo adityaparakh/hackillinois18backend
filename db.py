e5f5a778642ba69edff924e5665afb9c9db8fac3
Aditya Parakh <aparakh@hawk.iit.edu>
2018-02-25 00:38:59 -0600

    testing

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
-import pyodbc
+
+
+
+'''import pyodbc
 server='walkablebiserver1.database.windows.net'
 database='walkable'
 username='walkable'
@@ -9,3 +12,4 @@ cursor = cnxn.cursor()
 cursor.execute("INSERT INTO incident VALUES (40.7666,-90.8888, 'food','2016-02-12 00:00:00')")
 cursor.execute("INSERT INTO traffic VALUES ('adi', 40.7666,-90.8888, '2016-02-12 00:00:00')")
 cnxn.commit()
+'''
